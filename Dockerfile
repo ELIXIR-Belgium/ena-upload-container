f3abf0b2114b313fd4cc0a7f1be795b2b52ee204
bedroesb <bedro@psb.vib-ugent.be>
2020-08-05 22:25:30 +0200

    base

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+
+# Example Galaxy container
+#
+# to build the docker image, go to root of training repo and
+#    docker build -t containername -f Dockerfile .
+#
+# to run image:
+#    docker run -p "8080:80" -t  containername 
+
+FROM quay.io/bgruening/galaxy:20.05
+MAINTAINER ELIXIR Belgium
+
+ENV GALAXY_CONFIG_BRAND "ENA-upload"
+ENV ENABLE_TTS_INSTALL=True
+ENV WEBIN_ID="Webin-56059"
+ENV WEBIN_SECRET=".secret"
+
+ADD bin/docker-install-tutorials.sh /setup-workflow.sh
+ADD .secret .secret
+ADD tools.yaml tools.yaml 
+ADD /workflow /workflowDir
+
+RUN /setup-workflow.sh
---
+++ bin/docker-install-tutorials.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Enable Test Tool Shed
+echo "Enable installation from the Test Tool Shed."
+export GALAXY_CONFIG_TOOL_SHEDS_CONFIG_FILE=$GALAXY_HOME/tool_sheds_conf.xml
+
+. /tool_deps/_conda/etc/profile.d/conda.sh
+conda activate base
+
+if pgrep "supervisord" > /dev/null
+then
+    echo "System is up and running. Starting with the installation."
+    export PORT=80
+else
+    # start Galaxy
+    export PORT=8080
+    service postgresql start
+    install_log='galaxy_install.log'
+
+    # wait for database to finish starting up
+    STATUS=$(psql 2>&1)
+    while [[ ${STATUS} =~ "starting up" ]]
+    do
+      echo "waiting for database: $STATUS"
+      STATUS=$(psql 2>&1)
+      sleep 1
+    done
+
+    echo "starting Galaxy"
+    # Unset SUDO_* vars otherwise conda run chown based on that
+    sudo -E -u galaxy -- bash -c "unset SUDO_UID; \
+        unset SUDO_GID; \
+        unset SUDO_COMMAND; \
+        unset SUDO_USER; \
+        ./run.sh -d $install_log --pidfile galaxy_install.pid --http-timeout 3000"
+
+    galaxy_install_pid=`cat galaxy_install.pid`
+    galaxy-wait -g http://localhost:$PORT -v --timeout 120
+fi
+
+# Create the admin user if not already done
+# Starting with 20.05 this user is only created at first startup of galaxy
+# We need to create it here for Galaxy Flavors = installing from Dockerfile
+if [[ ! -z $GALAXY_DEFAULT_ADMIN_USER ]]
+    then
+        (
+        cd $GALAXY_ROOT
+        . $GALAXY_VIRTUAL_ENV/bin/activate
+        echo "Creating admin user $GALAXY_DEFAULT_ADMIN_USER with key $GALAXY_DEFAULT_ADMIN_KEY and password $GALAXY_DEFAULT_ADMIN_PASSWORD if not existing"
+        python /usr/local/bin/create_galaxy_user.py --user "$GALAXY_DEFAULT_ADMIN_EMAIL" --password "$GALAXY_DEFAULT_ADMIN_PASSWORD" \
+        -c "$GALAXY_CONFIG_FILE" --username "$GALAXY_DEFAULT_ADMIN_USER" --key "$GALAXY_DEFAULT_ADMIN_KEY"
+        )
+fi
+
+# install workflow
+echo " - Extracting tools from workflows"
+for w in `ls /workflowDir/*.ga | sort -r`
+do
+    echo "------ $w ------"
+    workflow-to-tools -w $w -o /workflowDir/wftools.yaml -l "Read cleaning"
+    echo " - Installing tools from workflow" 
+    n=0
+    until [ $n -ge 3 ]
+    do
+        shed-tools install -t /workflowDir/wftools.yaml -g "http://localhost:$PORT" -a fakekey && break
+        n=$[$n+1]
+        sleep 5
+        echo " - Retrying shed-tools install"
+    done    
+    echo " - Installing workflow"
+    workflow-install --publish_workflows --workflow_path $w -g "http://localhost:$PORT" -a fakekey
+    echo "Installed workflow $w"
+done
+
+echo "Installing extra tools" 
+n=0
+until [ $n -ge 3 ]
+do
+    shed-tools install -t tools.yaml -g "http://localhost:$PORT" -a fakekey && break
+    n=$[$n+1]
+    sleep 5
+    echo " - Retrying shed-tools install "
+done        
+
+
+exit_code=$?
+
+if [ $exit_code != 0 ] ; then
+    if [ "$2" == "-v" ] ; then
+        echo "Installation failed, Galaxy server log:"
+        cat $install_log
+    fi
+    exit $exit_code
+fi
+
+if ! pgrep "supervisord" > /dev/null
+then
+    # stop everything
+    sudo -E -u galaxy ./run.sh --stop --pidfile galaxy_install.pid
+    rm $install_log
+    service postgresql stop
+fi
